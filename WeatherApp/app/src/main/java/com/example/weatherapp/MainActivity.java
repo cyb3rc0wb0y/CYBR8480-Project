c7128928e74c2100fc234f418a8204a9ea4c291d
Jeff Smith <jeffreyasmith@unomaha.edu>
2019-11-18 20:41:55 -0600

    Update

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.view.View
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 

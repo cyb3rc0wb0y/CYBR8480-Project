6a9b9d629172327cb30a6948a537323001342bf9
Jeff Smith <jeffreyasmith@unomaha.edu>
2019-11-18 22:18:22 -0600

    Updated documentation

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -15,17 +15,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Button Documentation: https://developer.android.com/reference/android/widget/Button
         final Button buttonOW = findViewById(R.id.buttonOpenWeather);
         buttonOW.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+
+                // Volley Request Documentation: https://developer.android.com/training/volley/simple
             }
         });
 
         final Button buttonPT = findViewById(R.id.buttonPhoneTemp);
         buttonPT.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                // Sensor Manager Documentation: https://developer.android.com/reference/android/hardware/SensorManager.html
+
+                // Temp Sensor Documentation: https://developer.android.com/reference/android/hardware/Sensor.html#STRING_TYPE_AMBIENT_TEMPERATURE
                 final String STRING_TYPE_AMBIENT_TEMPERATURE = Sensor.STRING_TYPE_AMBIENT_TEMPERATURE;
+
+                // Toast Documentation: https://developer.android.com/guide/topics/ui/notifiers/toasts
                 Toast.makeText(getApplicationContext(), STRING_TYPE_AMBIENT_TEMPERATURE, Toast.LENGTH_LONG).show();
             }
         });
